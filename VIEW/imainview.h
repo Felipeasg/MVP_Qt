aa7463e18c2ff22929d169f547ab1b77a14d1b56
fadriano <felipeadrianosg@gmail.com>
2016-05-22 01:45:23 -0300

    Fixing the but declare inconsistent
    Signed-off-by: fadriano <felipeadrianosg@gmail.com>

--- VIEW/imainview.h
+++ VIEW/imainview.h
@@ -13,16 +13,16 @@ class IMainView
 public:
 
     //If you not use = 0 (error: Undefined reference to vtable)
-    virtual void setRegisterActionListener(IActionListener* listener) const = 0;
-    virtual void setNewUserRegisteredActionListener(IActionListener* listener) const = 0;
+    virtual void setRegisterActionListener(IActionListener* listener) = 0;
+    virtual void setNewUserRegisteredActionListener(IActionListener* listener) = 0;
 
-    virtual void setName(string name) const = 0;
-    virtual string getName() const = 0;
+    virtual void setName(string name) = 0;
+    virtual string getName() = 0;
 
-    virtual void setCPF(string cpf) const = 0;
-    virtual string getCPF() const = 0;
+    virtual void setCPF(string cpf) = 0;
+    virtual string getCPF() = 0;
 
-    virtual void newUserAdded(string name, string cpf) const = 0;
+    virtual void newUserAdded(string name, string cpf) = 0;
 };
 
 #endif // IMAINVIEW_H
--- main.cpp
+++ main.cpp
@@ -5,107 +5,10 @@
 
 using namespace std;
 
-class SortBehavior
-{
-public:
-    virtual void sort() const = 0;
-};
-
-class Merge: public SortBehavior
-{
-public:
-    virtual void sort() const {
-        qDebug() << "Merge sort()\n";
-    }
-};
-
-class Quick: public SortBehavior {
-public:
-    virtual void sort() const {
-        qDebug() << "Quick sort()\n";
-    }
-};
-
-class Heap: public SortBehavior
-{
-public:
-    virtual void sort() const {
-        qDebug() << "Heap sort()\n";
-    }
-};
-
-class SearchBehavior
-{
-public:
-    virtual void search() const = 0;
-};
-
-class Sequential: public SearchBehavior
-{
-public:
-    virtual void search() const {
-        qDebug() << "Sequential search()\n";
-    }
-};
-
-class BinaryTree: public SearchBehavior
-{
-public:
-    virtual void search() const {
-        qDebug() << "BinaryTree search()\n";
-    }
-};
-
-class HashTable: public SearchBehavior
-{
-public:
-    virtual void search() const {
-        qDebug() << "HashTable search()\n";
-    }
-};
-
-// Context
-class Collection
-{
-private:
-    SortBehavior* m_sort;
-    SearchBehavior* m_search;
-public:
-    Collection(){}
-    void set_sort(SortBehavior* s){
-        m_sort = s;
-    }
-    void set_search(SearchBehavior* s){
-        m_search = s;
-    }
-    void sort() const {
-        m_sort->sort();
-    }
-    void search() const {
-        m_search->search();
-    }
-};
-
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-
-    Merge merge;
-    Quick quick;
-    Heap heap;
-
-    Sequential sequential;
-    BinaryTree binaryTree;
-    HashTable hashTable;
-
-    Collection colA;
-    colA.set_sort(&merge);
-    colA.sort();
-
-    Collection colB;
-    colB.set_search(&binaryTree);
-    colB.search();
 
     w.show();
 
